1f3706b9d2ec37336296f44ebeeaff10a31a84f3
Harjit Khaira <hkhaira1@jh.edu>
2021-09-02 08:46:24 -0400

    day1hw

--- day1-evening/day1hw.py
+++ day1-evening/day1hw.py
@@ -3,13 +3,27 @@ count=0
 for line in f:
     if not line.startswith("@"):
         count = count + 1
-print(count)
+#print(count)
+
+f.close()
 #make an empty count, and count into it the alignments without anything that starts with @
 #6th column is the cigar, m is the alignment match which means 40 is the perfect aligment 
 
+f = open("SRR072893.sam")
+count2=0
 for m in f:
-    if m==40:
-        print(m)
+    if "40M" in m.strip().split():
+        count2+=1
+#print(count2)
+
+f.close()
+
+#count the indexes for the 3rd question
+f = open("SRR072893.sam")
+for i in f:
+    i.strip().split()==[4]
+    print(i)
+
         
 
     
